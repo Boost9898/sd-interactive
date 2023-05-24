8cd5f714e8d3a3f6d051ca8fc9b3930ee7afd140
Bram <bls.place@gmail.com>
2023-05-24 16:32:08 +0200

    API usage functions, make use of touchscreen function to pass data through

--- public/js/scripts/sd.js
+++ public/js/scripts/sd.js
@@ -2,6 +2,8 @@
 // IMPORTS
 // ************************************************** \\
 import { fadeIn, fadeOut } from './fades.js';
+import * as touchscreen from '../touchscreen.js';
+
 
 // ************************************************** \\
 // GLOBAL AND EXPORTED VARIABELS
@@ -29,85 +31,41 @@ export async function showModels() {
 // ************************************************** \\
 // TXT2IMG API REQUEST
 // ************************************************** \\
-export async function txt2img() {
-  let payload = {
-    prompt: 'Austronaut on a horse',
-    steps: 15,
-    width: 512,
-    height: 512
-  };
+// export async function txt2img() {
+//   let payload = {
+//     prompt: 'Austronaut on a horse',
+//     steps: 15,
+//     width: 512,
+//     height: 512
+//   };
 
-  try {
-    currentlyGenerating = true;
-    // fadeIn(loader, 1000);
-    const response = await fetch(`${api}/sdapi/v1/txt2img`, {
-      method: 'POST',
-      body: JSON.stringify(payload),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    generatedImageData = await response.json();
-    console.log(generatedImageData.images[0]);
-    // base64ToImage(generatedImageData.images[0]);
-    // removeImageOverlay();
-    // fadeOut(loader, 500);
-    currentlyGenerating = false;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-
-
-
-// // ************************************************** \\
-// // ENCODE BASE64 TO IMAGE
-// // place image in DOM (if already present, remove it
-// // ************************************************** \\
-// async function base64ToImage(arg) {
-//   const generatedImage = new Image();
-//   generatedImage.src = `data:image/png;base64,${arg}`;
-//   await generatedImage.decode().catch(() => { }); // wait until image is decoded and continue code execution
-//   document.getElementById("generatedImage").firstElementChild?.remove();
-//   document.getElementById("generatedImage").appendChild(generatedImage);
-// }
-
-// function setImageOverlay() {
-//   staticImage.style.backgroundImage = "url('images/image-01-mask-cut.png')";
-// }
-
-// function removeImageOverlay() {
-//   staticImage.style.backgroundImage = "none";
-// }
-
-
-
-// // ************************************************** \\
-// // GENERATE-TXT2IMG-BUTTON FUNCTIONALITY
-// // prevent overflow of image generation processes
-// // ************************************************** \\
-// document.getElementById('generate-txt2img-button').addEventListener('click', function () {
-//   if (!currentlyGenerating) {
-//     txt2img();
-//   } else {
-//     console.log('Currently generating image.')
+//   try {
+//     currentlyGenerating = true;
+//     // fadeIn(loader, 1000);
+//     const response = await fetch(`${api}/sdapi/v1/txt2img`, {
+//       method: 'POST',
+//       body: JSON.stringify(payload),
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     });
+//     generatedImageData = await response.json();
+//     console.log(generatedImageData.images[0]);
+//     // base64ToImage(generatedImageData.images[0]);
+//     // removeImageOverlay();
+//     // fadeOut(loader, 500);
+//     currentlyGenerating = false;
+//   } catch (error) {
+//     console.error(error);
 //   }
-// });
+// }
 
-
-// // ************************************************** \\
-// // SHOW-MODELS-BUTTON
-// // ************************************************** \\
-// document.getElementById('show-models-button').addEventListener('click', function () {
-//   showModels()
-// });
 
 // ************************************************** \\
-// IMG2IMG STUFF (TODO: comments)
+// IMG2IMG API AND HANDLING
 // ************************************************** \\
 
-export function img2img() {
+export function img2img(photoDataUrl) {
 
   console.log('sd img2img')
 
@@ -133,30 +91,78 @@ export function img2img() {
 
   // Load async image and mask, process them, and pass them to the img2imgapi() function
   async function loadImageAndMask() {
-    const [base64StringImage, base64StringMask] = await Promise.all([
+    const [base64StringImage, base64StringMask, base64StringtestPhoto] = await Promise.all([
       fetchBase64FromUrl('images/input/image-01.png'),
-      fetchBase64FromUrl('images/input/image-01-mask.png')
+      fetchBase64FromUrl('images/input/image-01-mask.png'),
     ]);
 
     // remove the prefix string (substring is more efficient although split shorter is)
     const image = base64StringImage.substring("data:image/png;base64,".length);
     const mask = base64StringMask.substring("data:image/png;base64,".length);
 
-    img2imgapi(image, mask);
+    // DEV: don't pass the mask since it's causing issues for now
+    img2imgapi(image, photoDataUrl, mask);
   }
 
   loadImageAndMask();
 
-  async function img2imgapi(initImages, mask) {
+  async function img2imgapi(image, photoDataUrl, mask) {
+
     const payload = {
+      init_images: [image],
+      resize_mode: 0,
+      denoising_strength: 0.7,
+      mask_blur: 36,
+      inpainting_fill: 0,
+      inpaint_full_res: true,
+      inpaint_full_res_padding: 72,
+      inpainting_mask_invert: 0,
+      initial_noise_multiplier: 1,
+      prompt: "young man, brown hair, portrait painting, detailed oil painting, renaissance, hyper realistic, 8k, detail, <lora:monet-wd14v10-000015:0.5>", // DEV make it customisable
+      negative_prompt: "EasyNegative:0.2",
+      seed: 1472411147,
+      sampler_name: "Euler",
+      sampler_index: "Euler",
+      batch_size: 1,
       steps: 10,
+      cfg_scale: 7,
       width: 512,
       height: 768,
-      denoising_strength: 2,
-      sampler_index: 'Euler',
-      prompt: 'young man, brown hair, portrait painting, detailed oil painting, renaissance, hyper realistic, 8k, detail, <lora:monet-wd14v10-000015:0.6>',
-      negative_prompt: 'EasyNegative:0.2',
-      init_images: [initImages],
+      restore_faces: false,
+      tiling: false,
+      send_images: true, // DEV
+      save_images: true, // DEV
+      alwayson_scripts: {
+        ControlNet: {
+          args: [
+            {
+              enabled: true,
+              input_image: photoDataUrl,
+              module: "canny",
+              model: "control_v11p_sd15_canny [d14c016b]",
+              weight: 1,
+              width: 512,
+              height: 768,
+              processor_res: 512,
+              resize_mode: "Crop and Resize",
+              lowvram: false,
+              control_mode: "Balanced", //My prompt is more important //ControlNet is more important //Balanced
+              pixel_perfect: true,
+              "threshold_a": 10,
+              "threshold_b": 220,
+              "guidance_start": 0.0,
+              "guidance_end": 1.0,
+              "pixel_perfect": true
+            },
+            {
+              enabled: false,
+              input_image: "",
+              module: "",
+              model: ""
+            }
+          ]
+        }
+      }
     };
 
     try {
@@ -168,12 +174,13 @@ export function img2img() {
         }
       });
       generatedImageData = await response.json();
-      console.log(generatedImageData)
+      touchscreen.catchGeneratedImageData(generatedImageData.images[0])
     } catch (error) {
       console.error(error);
     }
   }
 }
+
 
 // // ************************************************** \\
 // // GENERATE-IMG2IMG-BUTTON FUNCTIONALITY
